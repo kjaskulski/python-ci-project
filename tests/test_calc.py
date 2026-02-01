130780515aaa91ea5bb94bf26fa8b4298fa659aa
Kacper <kacjas001@pbs.edu.pl>
2026-02-01 01:42:32 +0100

    Python kalkulator z testami

---
+++ tests/test_calc.py
@@ -0,0 +1,15 @@
+import pytest
+from app.calculator import Calculator
+
+def test_add():
+    calc = Calculator()
+    assert calc.add(2, 2) == 4
+
+def test_divide():
+    calc = Calculator()
+    assert calc.divide(10, 2) == 5
+
+def test_divide_error():
+    calc = Calculator()
+    with pytest.raises(ValueError):
+	calc.divide(10, 0)
